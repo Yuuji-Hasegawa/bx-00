8c76a0fbe5316023d5b17703c4abfb4b51abe2d5
Yuuji Hasegawa <hasegawa@kamenwriter.com>
2022-06-28 22:14:19 +0900

    gtm追加

--- app/wp-content/themes/bx-00/header.php
+++ app/wp-content/themes/bx-00/header.php
@@ -4,6 +4,24 @@
 <head
     prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# <?php echo get_ogp_type();?>: http://ogp.me/ns/<?php echo get_ogp_type();?>#">
     <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.defer = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer',
+            '<?php echo get_vars('site', 'gtm');?>'
+        );
+    </script>
     <!-- End Google Tag Manager -->
     <meta
         charset="<?php bloginfo('charset');?>" />
@@ -13,6 +31,9 @@
 
 <body <?php body_class();?>>
     <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe
+            src="https://www.googletagmanager.com/ns.html?id=<?php echo get_vars('site', 'gtm');?>"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
     <header class="o-container o-container:header">
         <a class="o-cluster o-cluster:headLogo"
